30ea5f19db6972dcd11f0a5fc461e46faf4e54fd
synnah03 <61142074+synnah03@users.noreply.github.com>
2026-07-16 08:55:56 +0000

    Trust all proxies for correct HTTPS URL generation on Cloud Run

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
 
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
